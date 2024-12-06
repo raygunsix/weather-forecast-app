246b22f872cde10587fbcb6e82818e787d909755
Chris Reid <chris@raygun6.com>
2024-12-05 20:17:11 -0800

    return data based on kind

--- backend.py
+++ backend.py
@@ -9,7 +9,15 @@ def get_data(place, forecast_days=None, kind=None):
         f"appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    if kind == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if kind == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+
+    return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
