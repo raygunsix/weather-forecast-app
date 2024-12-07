938a8a1c2a2707d0bfdc71e1761e35a10a17f6d1
Chris Reid <chris@raygun6.com>
2024-12-06 17:27:53 -0800

    handle key error exceptions

--- backend.py
+++ backend.py
@@ -3,21 +3,19 @@ import requests
 
 API_KEY = os.getenv("API_KEY")
 
-def get_data(place, forecast_days=None, kind=None):
-    url = "http://api.openweathermap.org/data/2.5/forecast?" \
-        f"q={place}&" \
-        f"appid={API_KEY}"
-    response = requests.get(url)
-    data = response.json()
-    filtered_data = data["list"]
-    nr_values = 8 * forecast_days
-    filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
-
+def get_data(place, forecast_days=None):
+    try:
+        url = "http://api.openweathermap.org/data/2.5/forecast?" \
+            f"q={place}&" \
+            f"appid={API_KEY}"
+        response = requests.get(url)
+        data = response.json()
+        filtered_data = data["list"]
+        nr_values = 8 * forecast_days
+        filtered_data = filtered_data[:nr_values]
+    except KeyError:
+        filtered_data = []
     return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
+    print(get_data(place="Tokyo", forecast_days=3))
